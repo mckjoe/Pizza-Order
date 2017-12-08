03f7a8a6af25562cd73bd7940c5405b8da19ad45
joe mckinney <mckinney.a.joe@gmail.com>
2017-12-07 20:14:30 -0800

    add some work to the css file and also a little better functionality in js

--- js/script.js
+++ js/script.js
@@ -22,10 +22,41 @@ Pizza.prototype.basicCost = function() {
   return cost;
 }
 
+Pizza.prototype.toppingCost = function() {
+  var toppingCost = 0;
+  if (this.toppings.length === 1) {
+    toppingCost += 1;
+  } else if (this.toppings.length === 2) {
+    toppingCost += 2;
+  } else if (this.toppings.length === 3) {
+    toppingCost += 3;
+  } else if (this.toppings.length === 4) {
+    toppingCost += 4;
+  } else if (this.toppings.length === 5) {
+    toppingCost += 5;
+  } else if (this.toppings.length === 6) {
+    toppingCost += 6;
+  } else if (this.toppings.length === 7) {
+    toppingCost += 7;
+  } else if (this.toppings.length === 8) {
+    toppingCost += 8;
+  } else if (this.toppings.length === 9) {
+    toppingCost += 9;
+  } else if (this.toppings.length === 10) {
+    toppingCost += 10;
+  } else if (this.toppings.length === 11) {
+    toppingCost += 11;
+  } else if (this.toppings.length === 12) {
+    toppingCost += 12;
+  }
+  return toppingCost;
+}
+
 $(document).ready(function() {
   var toppings = [];
   $("#pizza").submit(function(event) {
     event.preventDefault();
+    // debugger;
     var size = $("#size").val();
     var crust = $("#crust").val();
     var newPizza = new Pizza(size, crust, toppings);
@@ -33,6 +64,14 @@ $(document).ready(function() {
       toppings.push($(this).val());
     });
     var flatRate = newPizza.basicCost();
-    console.log(flatRate);
+    $(".initTotal").text(flatRate);
+    var toppingTotal = newPizza.toppingCost();
+    $(".toppingTotal").text(toppingTotal);
+    var pizzaTotal = flatRate += toppingTotal;
+    $(".finalTotal").text(pizzaTotal);
+    $(".crust").text(crust);
+    $(".size").text(size);
+    $("#pizza").hide();
+    $(".receipt").show(2000);
   });
 });
