31c51f52f03ef5837592d3292f3e24f3948b96cd
joe mckinney <mckinney.a.joe@gmail.com>
2017-12-07 16:24:19 -0800

    add a prototype that will make a cost for each pizza size, and will add script to js/script.js to capture all of the user inputs into variables

--- js/script.js
+++ js/script.js
@@ -3,3 +3,31 @@ function Pizza(size, crust, toppings) {
   this.crust = crust;
   this.toppings = toppings;
 }
+
+Pizza.prototype.basicCost = function() {
+  var cost = 0;
+  if (this.size === "Small") {
+    cost += 5;
+  } else if (this.size === "Medium") {
+    cost += 7;
+  } else if (this.size === "Large") {
+    cost += 9;
+  } else if (this.size === "XLRG") {
+    cost += 10;
+  }
+  return cost;
+}
+
+$(document).ready(function() {
+  var toppings = [];
+  $("#pizza").submit(function(event) {
+    event.preventDefault();
+    var size = $("#size").val();
+    var crust = $("#crust").val();
+    var newPizza = new Pizza(size, crust, toppings);
+    $("input:checkbox[name=topping]:checked").each(function() {
+      toppings.push($(this).val());
+    });
+    console.log(newPizza);
+  });
+});
